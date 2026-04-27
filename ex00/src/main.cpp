a9999327f6beed98cdda80700c2d9808a30633d8
Serhii Veretennikov <gridis72@gmail.com>
2026-04-27 14:32:57 +0200

    feat: finished exercise

---
+++ ex00/src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "whatever.hpp"
+
+int main() {
+	int a = 3;
+	int b = 5;
+
+
+	std::cout << "a: " << a << "; b: " << b << std::endl;
+	swap(a, b);
+	std::cout << "a: " << a << "; b: " << b << std::endl;
+	std::cout << "min(a, b): " << min(a, b) << std::endl;
+	std::cout << "max(a, b): " << max(a, b) << std::endl;
+
+	std::string c = "yoyo";
+	std::string d = "heya";
+
+	std::cout << "c: " << c << "; d: " << d << std::endl;
+	swap(c, d);
+	std::cout << "c: " << c << "; d: " << d << std::endl;
+	std::cout << "min(c, d): " << min(c, d) << std::endl;
+	std::cout << "max(c, d): " << max(c, d) << std::endl;
+}
