62787b6bfe59f318058d32919057bf84b72c6ebf
Serhii Veretennikov <gridis72@gmail.com>
2026-05-06 14:56:40 +0200

    feat: added const array to main

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -13,12 +13,18 @@ void	printStr(std::string &val) {
 	std::cout << val << std::endl;
 }
 
+void	printConstInt(const int &val) {
+	std::cout << val << std::endl;
+}
+
 int main() {
 	int intArr[] = { 1, 2, 3, 4, 5 };
 	std::string stringArr[] = { "Hello", "There", "General", "Kenobi"};
+	const int constIntArr[] = { 1, 2, 3, 4, 5 };
 
 	iter(intArr, 5, printInt);
 	iter(intArr, 5, doubleInt);
 	iter(intArr, 5, printInt);
 	iter(stringArr, 4, printStr);
+	iter(constIntArr, 5, printConstInt);
 }
