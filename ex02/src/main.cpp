4c5b6e0b3aa32806ddbdd756243a93cf46a388d8
Serhii Veretennikov <gridis72@gmail.com>
2026-06-08 15:39:31 +0200

    feat: added segfault test

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -2,15 +2,24 @@
 #include <iostream>
 
 int main() {
+	Array<int> lol;
 	Array<int> a(5);
 	Array<int> b(a);
 	
+	std::cout << "lol size: " << lol.size() << std::endl;
+	try {
+		lol[0];
+	} catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << "a size: " << a.size() << std::endl;
 	for (int i = 0; i < 5; i++) {
 		a[i] = i;
 		std::cout << "a" << a[i] << std::endl;
 		std::cout << "b" << b[i] << std::endl;
 	}
 
+	std::cout << "c size: " << a.size() << std::endl;
 	Array<int> c = a;
 	for (int i = 0; i < 5; i++)
 		std::cout << "c" << c[i] << std::endl;
